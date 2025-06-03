81698e33acddb4ee8cc96e3bf76c29407d6bb783
AlysonEast <a.east.ecology@gmail.com>
2025-06-03 19:19:45 -0400

    uploading draft for workflow for running all ABtrays will rename the file when it is ready to be used

---
+++ NOTREADYYETrunAllABTrayProcesses.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=FirstpassRename
+#SBATCH --time=0:10:00 #10 minutes
+#SBATCH --mail-type=ALL
+#SBATCH --output=out_FirstpassRename.%j
+#SBATCH --account=PUOM0017
+
+module load R/4.4.0-gnu11.2
+
+#Manuall Processes:
+## Download from Google Drive and Upload to OSC
+### BeetleMetadata
+### catalog_firstPass_renamedMetadata
+### catalog_Belitz_renamedMetadataTemplate
+
+## Download and reopload to Google Drive as xlsx
+### /NEONIndividualLinkageChecks/QueryUnder/Checked/[all google sheets]
+### /NEONIndividualLinkageChecks/QueryOver/Checked/[all google sheets]
+
+#Run the rclone copy to pull over everything of OSC
+./rclone.sh
+
+sleep 5
+
+#Run all ABTray Processes in Sequence
+Rscript ./ABTrays/CheckBeetleCounts.R
+sleep 5
+./ABTrays/copyImagesRename.sh
+sleep 5
+
+#Run Secondary FirstPass Processes in Sequence
+Rscript ./FirstPass/CheckBeetleCounts.R
+sleep 5
+./FirstPass/copyImagesWithSecondRename.sh
+sleep 5
+
+#Run Secondary Belitz Processes in Sequence
+#Rscript ./Belitz/CheckBeetleCounts.R
+sleep 5
+#./Belitz/copyImagesWithSecondRename.sh
+sleep 5
+
+# NOT READY YET, but eventually:
+#Rscript collatIndividualsFromAllSources.R
+
