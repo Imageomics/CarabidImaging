38e89ccd83ed85d1017dfef64b05ada2eb339f70
AlysonEast <a.east.ecology@gmail.com>
2025-06-03 19:18:30 -0400

    adding a script to push individual linkage csvs that need manual checking down to the google drive so that they don't need to be manually exported and moved

---
+++ rclone_sendFilesToStudents.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Set remote name and path
+REMOTE="NEONIndividualLinkageChecks"
+
+# Set local destination
+LOCAL_DIR="/fs/ess/PAS2136/CarabidImaging/NEONIndividualLinkageChecks/" 
+
+# Use rclone to copy only new or updated files
+rclone copy "$LOCAL_DIR" "$REMOTE:" \
+  --max-depth 2 \
+  --include "*.csv" \
+  --verbose \
+  --log-file="rclone_send.log"
