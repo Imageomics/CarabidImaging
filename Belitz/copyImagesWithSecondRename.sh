8d48ef0bb6a8efc2cbcebaa17b325ab8e5a98959
AlysonEast <a.east.ecology@gmail.com>
2025-06-11 09:12:43 -0400

    updating awk col numbers based on df size

--- Belitz/copyImagesWithSecondRename.sh
+++ Belitz/copyImagesWithSecondRename.sh
@@ -13,7 +13,7 @@ DEST_DIR="$WORKDIR/Images/FinalImages/ABTrays"
 mkdir -p "$DEST_DIR"
 
 # Skip the header and process CSV lines
-#tail -n +2 "$CSV" | awk -F',' '{print $20","$1}' | while IFS=',' read -r imageID newImageID; do # awk number need to be updated
+tail -n +2 "$CSV" | awk -F',' '{print $23","$1}' | while IFS=',' read -r imageID newImageID; do # awk number need to be updated
     # Trim whitespace (optional but good practice)
     imageID=$(echo "$imageID" | xargs)
     newImageID=$(echo "$newImageID" | xargs)
